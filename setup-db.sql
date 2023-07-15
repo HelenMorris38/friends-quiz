d509ceba6d9e99f2576c5f193d10f16dc65084a0
Helen Morris <morris_helen@hotmail.com>
2023-07-15 15:41:08 +0100

    change name of id column to singular

--- setup-db.sql
+++ setup-db.sql
@@ -5,8 +5,9 @@ CREATE DATABASE friends_quiz;
 
 \c friends_quiz
 
+DROP TABLE IF EXISTS questions;
 CREATE TABLE questions (
-    questions_id SERIAL PRIMARY KEY,
+    question_id SERIAL PRIMARY KEY,
     question VARCHAR,
     answer VARCHAR
 );
