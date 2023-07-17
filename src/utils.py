52ba4ba96bf3bd2fa040d9f1978d0363be0c825c
Helen Morris <morris_helen@hotmail.com>
2023-07-17 13:56:26 +0100

    add function to turn df values into a list

--- src/utils.py
+++ src/utils.py
@@ -2,3 +2,9 @@ import pandas as pd
 
 def load_data_frame_from_csv(filepath):
     return pd.read_csv(f'{filepath}')
+
+def df_to_list_of_row_values(data_frame):
+    return data_frame.values.tolist()
+
+def insert_data(conn, data):
+    pass
