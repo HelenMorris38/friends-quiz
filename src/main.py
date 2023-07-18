cb861a6f150c18cde05cf91976199ac5c73e9402
Helen Morris <morris_helen@hotmail.com>
2023-07-18 11:45:03 +0100

    refactor to remove use of utils functions

--- src/main.py
+++ src/main.py
@@ -2,18 +2,19 @@ import psycopg2
 from src.utils import load_data_frame_from_csv, df_to_list_of_row_values
 from dotenv import load_dotenv
 from os import environ
+import pandas as pd
 
 load_dotenv()
 
-questions_df = load_data_frame_from_csv('friends_questions.csv')
-data = df_to_list_of_row_values(questions_df)
+questions_df = pd.read_csv('friends_questions.csv')
+data = questions_df.values.tolist()
+
 username = environ.get('PG_USERNAME')
 conn = psycopg2.connect(dbname="friends_quiz", user=username)
 
 cur = conn.cursor()
 
 cur.executemany("INSERT INTO questions (question, answer) VALUES (%s, %s);", data)
-
 
 conn.commit()
 conn.close()
