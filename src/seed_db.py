59b44357e7d885fb4e1ef4526f919348095fa188
Helen Morris <morris_helen@hotmail.com>
2023-07-18 14:36:55 +0100

    refactor to use utils functions

--- src/seed_db.py
+++ src/seed_db.py
@@ -1,16 +1,13 @@
 import psycopg2
-from src.utils import load_data_frame_from_csv, df_to_list_of_row_values
-from dotenv import load_dotenv
-from os import environ
+from src.utils import connect_to_db, get_username
 import pandas as pd
 
-load_dotenv()
 
 questions_df = pd.read_csv('friends_questions.csv')
 data = questions_df.values.tolist()
 
-username = environ.get('PG_USERNAME')
-conn = psycopg2.connect(dbname="friends_quiz", user=username)
+username = get_username()
+conn = connect_to_db(username)
 
 cur = conn.cursor()
 
