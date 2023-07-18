587cd43bdcc9a3d1f3c4d2fa4c5eb5d9c51b4c16
Helen Morris <morris_helen@hotmail.com>
2023-07-18 19:32:21 +0100

    start writing mock tests for connect to db

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,20 @@
 from src.utils import connect_to_db
+import pytest
+from unittest.mock import patch
 
+@pytest.fixture
+def mock_env():
+    env_vars = {
+        'PG_USERNAME': 'abc'
+    }
+    with patch.dict('os.environ', env_vars) as mock_env:
+        yield mock_env
 
-
+# needs mock db adding
+@patch('src.utils', autospec=True)
+def test_connect_to_db_creates_connection(mock_conn, mock_env):
+    connect_to_db()
+    mock_conn.assert_called_with(
+        dbname='test_db',
+        user='abc'
+    )
