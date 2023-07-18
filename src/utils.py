ff0565b346a7969dd70ae8218619ab97084f461a
Helen Morris <morris_helen@hotmail.com>
2023-07-18 14:37:12 +0100

    create connect to db and get username funcs

--- src/utils.py
+++ src/utils.py
@@ -1,20 +1,29 @@
-import pandas as pd
 import psycopg2
+from dotenv import load_dotenv
+from os import environ
 
+load_dotenv()
 
-def connect_to_db():
+def get_username():
     try:
-        conn = psycopg2.connect("dbname=friends_quiz user=helenmorris")
+        username = environ.get('PG_USERNAME')
+        return username
+    except:
+        return 'unable to get username'
+
+def connect_to_db(username):
+    try:
+        conn = psycopg2.connect(dbname="friends_quiz", user=username)
         return conn
     except:
         return 'unable to connect to database'
 
 
-def load_data_frame_from_csv(filepath):
-    return pd.read_csv(f'{filepath}')
+# def load_data_frame_from_csv(filepath):
+#     return pd.read_csv(f'{filepath}')
 
-def df_to_list_of_row_values(data_frame):
-    return data_frame.values.tolist()
+# def df_to_list_of_row_values(data_frame):
+#     return data_frame.values.tolist()
 
 def insert_data(conn, data):
     pass
